7c2c8415ee960ddd470db2383b919f59cbcfe024
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2023-03-19 03:08:45 +0530

    Create 1_Buy_Sell.cpp

---
+++ 1_Buy_Sell.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int buysell(vector<int>arr){
+    int buy,profit;
+    buy=INT_MAX;
+    for(int i=0;i<arr.size();i++){
+        if(buy<arr[i]){
+            profit=max(profit,arr[i]-buy);
+        }
+        else{
+            buy=arr[i];
+        }
+    }
+    return profit;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<buysell(arr);
+}
